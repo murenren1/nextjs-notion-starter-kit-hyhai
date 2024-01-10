9e3fe39d21d1a1ca61f05f20bb016aa5320a7c0c
HYH <59929648+murenren1@users.noreply.github.com>
2024-01-10 22:13:25 +0800

    基础设置修改

--- site.config.ts
+++ site.config.ts
@@ -2,15 +2,15 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '0ba1beade5cb4c8ab9920336f03683a4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  name: '何艺汇的博客',
+  domain: '何艺汇，正在AI创业，专注AI，专注个人成长',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
